470cbcc27dc13da25f9001e47b163bb946c3f358
joshcomstock9777-glitch <joshcomstock9777@gmail.com>
2026-08-09 19:20:31 -0400

    Add isolated V2 Firebase web configuration

---
+++ v2/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCcLtr8Ci2HKa-c9E-Ky2-XZoiUjNmF0ik",
+  authDomain: "moonshadow-wake-relay-poc.firebaseapp.com",
+  databaseURL: "https://moonshadow-wake-relay-poc-default-rtdb.firebaseio.com",
+  projectId: "moonshadow-wake-relay-poc",
+  storageBucket: "moonshadow-wake-relay-poc.firebasestorage.app",
+  messagingSenderId: "10900801081",
+  appId: "1:10900801081:web:78b796d45e05c2c69e2fe8"
+};
+
+window.__FIREBASE_CONFIG__ = firebaseConfig;
